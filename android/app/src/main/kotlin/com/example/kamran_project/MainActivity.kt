6b6b619b06a8996b7587a2f3dab8d7110874064e
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-21 11:02:46 +0500

    AppBar Done

---
+++ android/app/src/main/kotlin/com/example/kamran_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kamran_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text("First App",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+      ),
+    );
+  }
+}
