273e6c66104d203db744469d39f38d8fe3c179fc
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-25 10:54:53 +0500

    Polyline Done

--- lib/google_map_view.dart
+++ lib/google_map_view.dart
@@ -1,20 +1,39 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class GoogleMapView extends StatelessWidget {
+class GoogleMapView extends StatefulWidget {
   GoogleMapView({super.key});
 
+  @override
+  State<GoogleMapView> createState() => _GoogleMapViewState();
+}
+
+class _GoogleMapViewState extends State<GoogleMapView> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  Set<Marker> markers = {
-    const Marker(markerId: MarkerId('1'), position: LatLng(33.6995, 73.0363),
-    infoWindow: InfoWindow(
-      title: "Google Map Class"
-    ))
-  };
+  Map<PolylineId, Polyline> polylines = {};
+
+  List<LatLng> polylineCoordinates = [];
+
+  PolylinePoints polylinePoints = PolylinePoints();
+
+  String googleAPiKey = "AIzaSyB2kvOckQn8vJlqi8PPnbOzK5dCf-xg3eQ";
+
+  double _originLatitude = 33.6995, _originLongitude = 73.0363;
+
+  double _destLatitude = 32.9425, _destLongitude = 73.7257;
+
+
+  @override
+  void initState() {
+    _getPolyline();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +43,43 @@ class GoogleMapView extends StatelessWidget {
       ),
       body: GoogleMap(
         mapType: MapType.normal,
-        markers: markers,
+        // markers: markers,
+
+        polylines: Set<Polyline>.of(polylines.values),
         initialCameraPosition: const CameraPosition(
           target: LatLng(33.6995, 73.0363),
-          zoom: 14.4746,
+          zoom: 12.4746,
         ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
     );
+  }
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("1");
+    Polyline polyline = Polyline(
+        polylineId: id, color: Colors.red, points: polylineCoordinates);
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: googleAPiKey,
+      request: PolylineRequest(
+        origin: PointLatLng(_originLatitude, _originLongitude),
+        destination: PointLatLng(_destLatitude, _destLongitude),
+        mode: TravelMode.driving,
+        wayPoints: [PolylineWayPoint(location: "Way to Jehlum")],
+      ),
+    );
+    if (result.points.isNotEmpty) {
+      result.points.map((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    _addPolyLine();
   }
 }
